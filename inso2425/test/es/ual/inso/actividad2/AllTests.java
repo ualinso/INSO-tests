0795b6d3bf45c69560295cb5d88c00197f0f32f5
Javier Criado <javi.criado@ual.es>
2024-10-28 15:39:23 +0100

    Tests for A2

---
+++ inso2425/test/es/ual/inso/actividad2/AllTests.java
@@ -0,0 +1,17 @@
+package es.ual.inso.actividad2;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ 
+  TestA2E1.class, 
+  TestA2E2.class,
+  TestA2E3.class,
+  TestA2E4.class,
+  TestA2E5.class
+})
+public class AllTests {
+  
+}
