5e7e22abdf69ba9de200210089f6802952203f02
Javier Criado <javi.criado@ual.es>
2025-11-24 10:26:45 +0100

    Tets for A3

---
+++ inso2526/test/es/ual/inso/actividad3/AllTests.java
@@ -0,0 +1,14 @@
+package es.ual.inso.actividad3;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ 
+  TestA3E1.class,
+  TestA3E2.class
+})
+public class AllTests {
+  
+}
